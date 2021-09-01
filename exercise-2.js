2e475f1845ef61c8695491d493f242d0e1fb1c98
Francesco Di Maria <dimaria.francesco@outlook.it>
2021-09-01 19:58:51 +0200

    Initial

---
+++ exercise-2.js
@@ -0,0 +1,15 @@
+function canPlay() {
+  const personName = 'Paul';
+
+  if (true) {
+    personName = 'George';
+  }
+
+  personName += ' plays football';
+
+  console.log(personName);
+}
+
+canPlay();
+
+
